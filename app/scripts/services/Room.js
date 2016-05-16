7c49b4a4fc6f1f40b2aeb4d4afca31e1d0ec5c35
divyasripadham <divya_sri16@yahoo.com>
2016-05-16 13:45:01 -0400

    Story 3 List messages:query first commit

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,21 +1,21 @@
 (function() {
   function Room($firebaseArray) {
+    var Room = {};
+
     var firebaseRef = new Firebase("https://torrid-fire-7225.firebaseio.com/");
     var rooms = $firebaseArray(firebaseRef.child('rooms'));
-    var messages = $firebaseArray(firebaseRef.child('messages'));
+    var messages = $firebaseArray(firebaseRef.child('messages').orderByChild('roomId').equalTo('room1'));
 
-    return {
-      all: rooms,
-      addRoom: function(newRoom) {
+    Room.all = rooms;
+    Room.addRoom = function(newRoom) {
         rooms.$add(newRoom);
-      },
-      addMessage: function(roomId, message) {
-
-      },
-      getMessages: function(roomId) {
-
-      }
     };
+    Room.getMessages = function(roomId) {
+      // var messages = $firebaseArray(firebaseRef.child('messages').orderByChild('roomId').equalTo(roomId));
+        console.log(messages);
+    };
+
+    return Room;
   }
 
   angular
