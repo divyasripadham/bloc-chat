01c9ace92977d76b8dd289be8cba16af48b64c56
divyasripadham <divya_sri16@yahoo.com>
2016-05-17 12:29:26 -0400

    Story 3 List messages:able to list messages

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,17 +1,18 @@
 (function() {
      function HomeCtrl($scope, Room, $uibModal) {
-        this.allRooms = Room.all;
-        this.open = function() {
+        var ctrl = this;
+        ctrl.allRooms = Room.all;
+        ctrl.open = function() {
           var modalInstance = $uibModal.open({
             templateUrl: '/templates/modal.html',
             controller: 'ModalInstanceCtrl',
             size: 'sm',
           });
         };
-        this.getMessagesFor = function(room) {
+        ctrl.getMessagesFor = function(room) {
           $scope.activeRoom = room;
           console.log("room id "+$scope.activeRoom.$id);
-          Room.getMessages($scope.activeRoom.$id);
+          ctrl.messages = Room.getMessages($scope.activeRoom.$id);
         }
      }
 
--- dist/scripts/services/Room.js
+++ dist/scripts/services/Room.js
@@ -4,15 +4,15 @@
 
     var firebaseRef = new Firebase("https://torrid-fire-7225.firebaseio.com/");
     var rooms = $firebaseArray(firebaseRef.child('rooms'));
-    var messages = $firebaseArray(firebaseRef.child('messages').orderByChild('roomId').equalTo('room1'));
 
     Room.all = rooms;
     Room.addRoom = function(newRoom) {
         rooms.$add(newRoom);
     };
     Room.getMessages = function(roomId) {
-      // var messages = $firebaseArray(firebaseRef.child('messages').orderByChild('roomId').equalTo(roomId));
+      var messages = $firebaseArray(firebaseRef.child('messages').orderByChild('roomId').equalTo(roomId));
         console.log(messages);
+        return messages;
     };
 
     return Room;
